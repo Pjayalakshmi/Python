4f29594a00cdabc072b901bb264528a9036a81e8
Pjayalakshmi <Jayalakshmi.cse18@gmail.com>
2014-01-20 19:52:17 +0530

    Create html.py

---
+++ html.py
@@ -0,0 +1,9 @@
+#!/python33/python
+print("content-type:text/html/")
+print(" ")
+print("<html><head>")
+print(" ")
+print("</head><body>")
+print("hello")
+print("<br>welcome")
+print("</body></html>")
