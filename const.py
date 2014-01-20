d00e0c1f4886cdc066df6d32ef284b41f6c6ce60
Pjayalakshmi <Jayalakshmi.cse18@gmail.com>
2014-01-20 19:48:56 +0530

    Create const.py

---
+++ const.py
@@ -0,0 +1,13 @@
+class student:
+ name="xxxx"
+ mark=90
+def--init--(self,a,b):
+self.name=a
+self.mark=b
+ print("constructor called")
+def display(self)
+ print("student name is{0}\n student mark is{1}".format(self.name,self.mark))
+obj=student('xxxx',90)
+obj.name="xxxx"
+obj.mark=90
+obj.display()
